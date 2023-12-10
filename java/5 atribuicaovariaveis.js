586f3a795abf1477f8057755072f0d8a7855983c
larissa <130381560+larissa2006kskskskskkssks@users.noreply.github.com>
2023-12-10 16:33:07 -0300

    5 atribuicaovariaveis.js

---
+++ java/5 atribuicaovariaveis.js
@@ -0,0 +1,19 @@
+console.log("trabalhando com atribuiçao de  variaveis");
+const idade = 16;
+const PrimeiroNome = "Ronaldo";
+const sobrenome = "Junior";
+
+//console.log(nome + " " + sobrenome);
+console.log(PrimeiroNome, sobrenome);
+console.log(`meu nome é ${PrimeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = PrimeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variavel
+idade = 16; //atribuindo valor
+idade = idade+1;
+console.log(idade);
