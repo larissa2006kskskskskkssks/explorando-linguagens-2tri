028371f9cdf2fbc67c82e9b4adbf89bbc3351b9f
larissa <130381560+larissa2006kskskskskkssks@users.noreply.github.com>
2023-12-10 16:27:14 -0300

    2 trabalhandoComvariaveis.js

---
+++ java/2 trablhandoComvariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("trabalhando com variaveis");
+console.log("trabalhando com variaveis");
+// JS é case sensitive
+
+const idade = 17;
+const nome = "larissa";
+
+//nao fazer
+//mes = "janeiro";
