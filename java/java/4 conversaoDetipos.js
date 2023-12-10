8e6282a51bdd1f816de9e16e93b44abc03a4dad1
larissa <130381560+larissa2006kskskskskkssks@users.noreply.github.com>
2023-12-10 16:30:45 -0300

    4 conversaoDetipos.js

---
+++ java/java/4 conversaoDetipos.js
@@ -0,0 +1,11 @@
+console.log("Conversao de Tipos");
+
+console.log("ano" + 2023);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("7" / "2");
+console.log("Ricardo" / "2"); //Not Number
+
+console.log(6.5);
+console.log(6,5);
